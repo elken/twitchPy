8e58ad3c7fa831225918fe6870be77e6aa7393c8
Ellis Kenyo <elken.tdos@gmail.com>
2015-04-01 21:42:58 +0100

    Lots of changes (Haven't pushed in a while)

---
+++ TODO.md
@@ -0,0 +1,14 @@
+# Working on
+- More verbose JSON junk (list of dictionaries, or some custom JSON)
+    - Current game
+    - Anything else of use
+- Clean up requests
+- More functional
+- Comments
+- START WRITING TESTS
+
+# TODO
+- Move to Gtk
+- Make it xplatform
+
+# DONE
--- gui.py
+++ gui.py
@@ -1,14 +1,22 @@
 from gi.repository import Gtk
 
+class Handler:
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
 class MainWindow(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Twitch client")
-
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file("main.glade")
-
     def run(self):
-        window = self.builder.get_object("mainWindow")
-        window.connect("delete-event", Gtk.main_quit)
+        builder = Gtk.Builder()
+        builder.add_from_file("main.glade")
+        builder.connect_signals(Handler())
+
+        label1 = Gtk.Label("Panel label")
+        label2 = Gtk.Label("Panel label2")
+        panel = builder.get_object("mainPanel")
+
+        panel.pack1(label1)
+        panel.pack2(label2)
+
+        window = builder.get_object("mainWindow")
         window.show_all()
         Gtk.main()
